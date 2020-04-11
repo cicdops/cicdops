e2014dab2c914bb255a2cf0ddb41f1ec51429c68
itech001 <itech001@126.com>
2020-04-11 15:34:59 +0000

    add gridsome site

---
+++ gridsome.config.js
@@ -0,0 +1,85 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Docc',
+  icon: {
+    favicon: './src/assets/favicon.png',
+    touchicon: './src/assets/favicon.png'
+  },
+  siteUrl: (process.env.SITE_URL ? process.env.SITE_URL : 'https://example.com'),
+  settings: {
+    web: process.env.URL_WEB || false,
+    twitter: process.env.URL_TWITTER || false,
+    github: process.env.URL_GITHUB || false,
+    nav: {
+      links: [
+        { path: '/docs/', title: 'Docs' }
+      ]
+    },
+    sidebar: [
+      {
+        name: 'docs',
+        sections: [
+          {
+            title: 'Getting Started',
+            items: [
+              '/docs/',
+              '/docs/installation/',
+              '/docs/writing-content/',
+              '/docs/deploying/',
+            ]
+          },
+          {
+            title: 'Configuration',
+            items: [
+              '/docs/settings/',
+              '/docs/sidebar/',
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        baseDir: './content',
+        path: '**/*.md',
+        typeName: 'MarkdownPage',
+        remark: {
+          externalLinksTarget: '_blank',
+          externalLinksRel: ['noopener', 'noreferrer'],
+          plugins: [
+            '@gridsome/remark-prismjs'
+          ]
+        }
+      }
+    },
+
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      options: {
+        tailwindConfig: './tailwind.config.js',
+        purgeConfig: {
+          // Prevent purging of prism classes.
+          whitelistPatternsChildren: [
+            /token$/
+          ]
+        }
+      }
+    },
+
+
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {  
+      }
+    }
+
+  ]
+}
